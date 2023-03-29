c1c609a96a1d6c98bf0a9fac31332562ce5e9bd8
Martin Studer <martin.studer@medi.ch>
2023-03-29 07:32:34 +0200

    ...

--- app/bin/install-dependencies.sh
+++ app/bin/install-dependencies.sh
@@ -5,7 +5,7 @@ set -e
 installDependency() {
     mkdir -p "/opt/flux-eco-node-http-server" &&
     cd "/opt/flux-eco-node-http-server" &&
-    wget  -O - "https://github.com/flux-eco/flux-eco-node-http-server/archive/refs/tags/v2023-03-29.tar.gz" |  tar -xz --strip-components=1
+    wget "https://github.com/flux-eco/flux-eco-node-http-server/archive/refs/tags/v2023-03-29.tar.gz" |  tar -xz --strip-components=1
 }
 
 installDependency
